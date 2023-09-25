9df59e80fa27af8ecf7639b86fbd33fb8f03aa98
HamzaMeh <wnadeem@ap.org>
2023-09-25 17:04:38 +0500

    Profile View alignment updated

--- app/src/main/java/com/arch/profileuicompose/ProfileUI.kt
+++ app/src/main/java/com/arch/profileuicompose/ProfileUI.kt
@@ -20,7 +20,6 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -34,15 +33,15 @@ fun ProfileUi() {
     Scaffold(
         modifier = Modifier.drawBehind {
             val p1 = Offset(size.width / 2f, 1200f) // Bottom vertex
-            val p2 = Offset(0f, 395f)                    // Top-left vertex
-            val p3 = Offset(size.width, 395f)
+            val p2 = Offset(0f, 380f)                    // Top-left vertex
+            val p3 = Offset(size.width, 380f)
 
             // Draw the rounded top rectangle on the canvas
             drawRoundRect(
                 color = DarkGreen,
                 topLeft = Offset(0f, 0f),
                 size = Size(size.width, 150.dp.toPx()),
-                cornerRadius = CornerRadius(x = 20f,y= 20F)
+                cornerRadius = CornerRadius(x = 20f, y = 20F)
             )
 
             // Create a Path for the inverted triangle
@@ -59,7 +58,7 @@ fun ProfileUi() {
                 color = DarkGreen
             )
         },
-       backgroundColor = Color.Transparent,
+        backgroundColor = Color.Transparent,
         topBar = {
             TopAppBar(
                 title = {
@@ -72,12 +71,20 @@ fun ProfileUi() {
                 },
                 actions = {
                     IconButton(onClick = { }) {
-                        Icon(imageVector = Icons.Default.MoreVert, contentDescription = "More",tint = Color.White)
+                        Icon(
+                            imageVector = Icons.Default.MoreVert,
+                            contentDescription = "More",
+                            tint = Color.White
+                        )
                     }
                 },
                 navigationIcon = {
                     IconButton(onClick = {}) {
-                        Icon(imageVector = Icons.Default.ArrowBack, contentDescription = "Back", tint = Color.White)
+                        Icon(
+                            imageVector = Icons.Default.ArrowBack,
+                            contentDescription = "Back",
+                            tint = Color.White
+                        )
                     }
                 },
                 backgroundColor = Color.Transparent,
@@ -95,6 +102,7 @@ fun ProfileUi() {
             Header("Mark Wood", "London, UK")
             FollowersLike()
             Friends()
+            Photos()
             Spacer(modifier = Modifier.padding(16.dp))
         }
 
@@ -113,74 +121,87 @@ fun Header(name: String, location: String) {
     var textColor by remember {
         mutableStateOf(Color.White)
     }
-    Card(
+    Box(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(start = 10.dp, top = 40.dp, end = 10.dp),
-        elevation = 4.dp,
-        shape = RoundedCornerShape(4.dp)
+            .padding(start = 10.dp, top = 40.dp, end = 10.dp)
+            .background(Color.Transparent)
     ) {
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 40.dp),
+            shape = RoundedCornerShape(4.dp),
+            elevation = 4.dp
+        ) {
 
-        Column(horizontalAlignment = Alignment.CenterHorizontally) {
-            Image(
-                painterResource(id = R.drawable.gerald_g),
-                contentDescription = "Profile",
-                modifier = Modifier
-                    .clip(
-                        CircleShape
-                    )
-                    .size(60.dp),
-                alignment = Alignment.TopCenter,
-                contentScale = ContentScale.Fit
-            )
-            Text(text = name, style = Typography.h1, modifier = Modifier.padding(top = 15.dp))
-
-            Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier.padding(4.dp)) {
-                Icon(
-                    imageVector = Icons.Default.LocationOn,
-                    contentDescription = "Location",
-                    modifier = Modifier
-                        .alpha(0.5f)
-                        .size(20.dp)
-                )
-                Text(
-                    text = location,
-                    style = Typography.body2,
-                    modifier = Modifier
-                        .padding(start = 2.dp)
-                        .alpha(0.7f),
-                )
-
-            }
-
-            Button(
-                onClick = {
-                    text = if (text == "Follow") {
-                        bgColor = Color.White
-                        textColor = Color.Black
-                        "Following"
-                    } else {
-                        bgColor = DarkGreen
-                        textColor = Color.White
-                        "Follow"
-                    }
-                },
-                modifier = Modifier
-                    .padding(vertical = 20.dp),
-                colors = ButtonDefaults.buttonColors(bgColor),
-                shape = RoundedCornerShape(30.dp),
-                border = if (bgColor == Color.White) BorderStroke(1.dp, DarkGreen) else null
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.padding(top = 30.dp)
             ) {
-                Text(
-                    text = text,
-                    style = Typography.h1,
-                    color = textColor,
-                    modifier = Modifier.padding(vertical = 5.dp, horizontal = 30.dp)
-                )
-            }
 
+                Text(text = name, style = Typography.h1, modifier = Modifier.padding(top = 15.dp))
+
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.padding(4.dp)
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.LocationOn,
+                        contentDescription = "Location",
+                        modifier = Modifier
+                            .alpha(0.5f)
+                            .size(20.dp)
+                    )
+                    Text(
+                        text = location,
+                        style = Typography.body2,
+                        modifier = Modifier
+                            .padding(start = 2.dp)
+                            .alpha(0.7f),
+                    )
+
+                }
+
+                Button(
+                    onClick = {
+                        text = if (text == "Follow") {
+                            bgColor = Color.White
+                            textColor = Color.Black
+                            "Following"
+                        } else {
+                            bgColor = DarkGreen
+                            textColor = Color.White
+                            "Follow"
+                        }
+                    },
+                    modifier = Modifier
+                        .padding(vertical = 20.dp),
+                    colors = ButtonDefaults.buttonColors(bgColor),
+                    shape = RoundedCornerShape(30.dp),
+                    border = if (bgColor == Color.White) BorderStroke(1.dp, DarkGreen) else null
+                ) {
+                    Text(
+                        text = text,
+                        style = Typography.h1,
+                        color = textColor,
+                        modifier = Modifier.padding(vertical = 5.dp, horizontal = 30.dp)
+                    )
+                }
+
+            }
         }
 
+        Image(
+            painterResource(id = R.drawable.gerald_g),
+            contentDescription = "Profile",
+            modifier = Modifier
+                .clip(
+                    CircleShape
+                )
+                .size(75.dp)
+                .align(Alignment.TopCenter),
+        )
     }
 }
 
@@ -275,5 +296,32 @@ fun FriendBox(drawable: Int, name: String) {
             modifier = Modifier.size(50.dp)
         )
         Text(name, style = Typography.body2)
+    }
+}
+
+@Composable
+fun Photos() {
+    Card(
+        modifier = Modifier
+            .padding(10.dp)
+            .fillMaxWidth(),
+        elevation = 4.dp,
+        shape = RoundedCornerShape(4.dp)
+    ) {
+        Column(modifier = Modifier.padding(horizontal = 20.dp)) {
+            Text(
+                text = "Photos",
+                style = Typography.h6,
+                modifier = Modifier.padding(vertical = 20.dp)
+            )
+            Image(
+                modifier = Modifier
+                    .padding(bottom = 20.dp)
+                    .clip(RoundedCornerShape(8.dp)),
+                painter = painterResource(id = R.drawable.image),
+                contentDescription = ""
+            )
+        }
+
     }
 }
